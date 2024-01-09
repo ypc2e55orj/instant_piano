d609cbab6998492d1364ec32f04c69b872b6d8e0
ypc2e55orj <ypc2e55orj@gmail.com>
2024-01-09 10:45:13 +0900

    Refactor

--- firmware/src/chord.c
+++ firmware/src/chord.c
@@ -9,7 +9,7 @@
 #define CHORD_SINE_NUM_SAMPLES 8191
 
 // typedef
-typedef struct CHORD
+struct Chord
 {
   uint32_t pwmMaxDuty;
   uint32_t pwmIsrFreq;
@@ -17,7 +17,7 @@ typedef struct CHORD
   uint16_t sinSkipCount[CHORD_NOTE_NUM];
   uint16_t sinSkipIndex[CHORD_NOTE_NUM];
   uint16_t notes;
-} Chord;
+};
 typedef double (*CalculateNoteFrequencyType)(int numOctave, enum Note note);
 
 // prototype
@@ -27,12 +27,7 @@ static double Chord_CalculateNoteFrequencyPythagoreanTuning(int numOctave, enum 
 static void Chord_UpdateDutyIsr(uint32_t status, uintptr_t context);
 
 // variables
-static volatile Chord chord = {0};
-static const CalculateNoteFrequencyType Chord_CalculateNoteFrequency[CHORD_FREQ_RATIO_NUM] = {
-    Chord_CalculateNoteFrequencyEqualTemperament,
-    Chord_CalculateNoteFrequencyJustIntonation,
-    Chord_CalculateNoteFrequencyPythagoreanTuning,
-};
+static volatile struct Chord chord = {0};
 
 void Chord_Initialize(uint32_t baseClockFreq, uint32_t pwmMaxDuty)
 {
@@ -50,6 +45,12 @@ void Chord_Initialize(uint32_t baseClockFreq, uint32_t pwmMaxDuty)
 
 void Chord_SetScale(enum FrequencyRatio freqRatio, int numOctave)
 {
+  static const CalculateNoteFrequencyType Chord_CalculateNoteFrequency[CHORD_FREQ_RATIO_NUM] = {
+      Chord_CalculateNoteFrequencyEqualTemperament,
+      Chord_CalculateNoteFrequencyJustIntonation,
+      Chord_CalculateNoteFrequencyPythagoreanTuning,
+  };
+
   if (freqRatio < 0 || CHORD_FREQ_RATIO_NUM <= freqRatio)
     return;
 
@@ -104,18 +105,21 @@ static double Chord_CalculateNoteReferenceFrequency(int numOctave)
 {
   return 27.5 * pow(2.0, numOctave);
 }
+
 static double Chord_CalculateNoteFrequencyEqualTemperament(int numOctave, enum Note note)
 {
-  const int noteOffset = 3;
+  static const int noteOffset = 3;
   return (pow(exp(1.0 / 12.0 * log(2.0)), note + noteOffset) * Chord_CalculateNoteReferenceFrequency(numOctave));
 }
+
 static double Chord_CalculateNoteFrequencyJustIntonation(int numOctave, enum Note note)
 {
-  double top[CHORD_NOTE_NUM] = {1.0, 16.0, 9.0, 6.0, 5.0, 4.0, 64.0, 3.0, 8.0, 5.0, 16.0, 15.0, 2.0};
-  double bottom[CHORD_NOTE_NUM] = {1.0, 15.0, 8.0, 5.0, 4.0, 3.0, 45.0, 2.0, 5.0, 3.0, 9.0, 8.0, 1.0};
+  static const double top[CHORD_NOTE_NUM] = {1.0, 16.0, 9.0, 6.0, 5.0, 4.0, 64.0, 3.0, 8.0, 5.0, 16.0, 15.0, 2.0};
+  static const double bottom[CHORD_NOTE_NUM] = {1.0, 15.0, 8.0, 5.0, 4.0, 3.0, 45.0, 2.0, 5.0, 3.0, 9.0, 8.0, 1.0};
   double refFreq = 6.0 / 5.0 * Chord_CalculateNoteReferenceFrequency(numOctave);
   return top[note] / bottom[note] * refFreq;
 }
+
 static double Chord_CalculateNoteFrequencyPythagoreanTuning(int numOctave, enum Note note)
 {
   // not implemented yet
